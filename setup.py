01e2023057a210e3180ec259ca54e96a694b8fd0
abhilashajay-dev <55262545+abhilashajay-dev@users.noreply.github.com>
2023-05-10 14:47:01 +0530

    git init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A Machine learning Model for predicting churn rates of a buisness ',
+    author='Abhilash Ajay',
+    license='BSD-3',
+)
